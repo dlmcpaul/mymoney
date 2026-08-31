27015f2e9db2630fce672fff18c02284a881c2ab
David McPaul <dlmcpaul@gmail.com>
2026-08-31 14:25:59 +1000

    Remove session data storage and handling.

--- src/main/java/com/hz/mymoney/ui/controllers/UpdatesController.java
+++ src/main/java/com/hz/mymoney/ui/controllers/UpdatesController.java
@@ -19,7 +19,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.FragmentsRendering;
 
@@ -29,7 +28,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@SessionAttributes("profitLossMonth")
 @RequiredArgsConstructor
 @Log4j2
 public class UpdatesController {
