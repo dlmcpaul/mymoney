8ec8afc8d08625873a7cfdbb224d9f350ddd41c7
David McPaul <dlmcpaul@gmail.com>
2026-08-31 14:23:00 +1000

    Make release properties final

--- src/main/java/com/hz/mymoney/MyMoneyApplication.java
+++ src/main/java/com/hz/mymoney/MyMoneyApplication.java
@@ -1,11 +1,14 @@
 package com.hz.mymoney;
 
+import com.hz.mymoney.configuration.ReleaseProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@EnableConfigurationProperties(ReleaseProperties.class)
 public class MyMoneyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hz/mymoney/configuration/ReleaseProperties.java
+++ src/main/java/com/hz/mymoney/configuration/ReleaseProperties.java
@@ -2,13 +2,15 @@ package com.hz.mymoney.configuration;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(value = "classpath:/release.properties", ignoreResourceNotFound = true)
-@Configuration
 @ConfigurationProperties("release")
 @Data
 public class ReleaseProperties {
-	private String version;
+	private final String version;
+
+	public ReleaseProperties(String version) {
+		this.version = version;
+	}
 }
